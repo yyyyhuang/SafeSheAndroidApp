53c9000a82c95605feba719af17f89670fbc2853
yyyyhuang <yingjie.huang2013@gmail.com>
2023-02-14 11:12:20 -0800

    button added

--- app/src/main/java/edu/northeastern/numad23team8/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23team8/MainActivity.java
@@ -2,13 +2,25 @@ package edu.northeastern.numad23team8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        api = (Button) findViewById(R.id.api);
+
+        api.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ApiActivity.class));
+            }
+        });
     }
 }
