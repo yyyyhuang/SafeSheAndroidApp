df0e2d21265088dbbba9bd4f3e1423e2043b4234
yyyyhuang <yingjie.huang2013@gmail.com>
2023-02-27 10:32:17 -0800

    about activity done

--- app/src/main/java/edu/northeastern/numad23team8/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23team8/MainActivity.java
@@ -9,17 +9,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button api;
+    private Button about;
+    private Button stickit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         api = (Button) findViewById(R.id.api);
+        about = (Button) findViewById(R.id.about);
 
         api.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ApiActivity.class));
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
             }
         });
     }
