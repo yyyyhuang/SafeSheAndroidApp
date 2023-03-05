e6000f22fc890df94e01a938465289c5260e1532
Xingjian-Bi <harry1672869@gmail.com>
2023-03-05 12:01:35 -0800

    added send sticker feature, need to finish add sticker count

--- app/src/main/java/edu/northeastern/numad23team8/ChatView.java
+++ app/src/main/java/edu/northeastern/numad23team8/ChatView.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -46,13 +47,23 @@ public class ChatView extends AppCompatActivity {
     RecyclerView messagerc;
     ArrayList<Message> messageArrayList;
 
+    ImageButton sticker0, sticker1, sticker2, sticker3, sticker4, sticker5;
+    String countSticker0, countSticker1, countSticker2, countSticker3, countSticker4, countSticker5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_view);
 
-        message = findViewById(R.id.gchat_message);
-        send = findViewById(R.id.gchat_send);
+//        message = findViewById(R.id.gchat_message);
+//        send = findViewById(R.id.gchat_send);
+
+        sticker0 = findViewById(R.id.sticker0);
+        sticker1 = findViewById(R.id.sticker1);
+        sticker2 = findViewById(R.id.sticker2);
+        sticker3 = findViewById(R.id.sticker3);
+        sticker4 = findViewById(R.id.sticker4);
+        sticker5 = findViewById(R.id.sticker5);
 
         messageArrayList = new ArrayList<>();
         messagerc = findViewById(R.id.rcmessage);
@@ -63,7 +74,7 @@ public class ChatView extends AppCompatActivity {
         intent = getIntent();
 
         sendername = getIntent().getStringExtra("sendername");
-        receivername=getIntent().getStringExtra("receivername");
+        receivername= getIntent().getStringExtra("receivername");
 
         messagerc.setLayoutManager(layoutManager);
         messageAdapter = new MessageAdapter(ChatView.this, messageArrayList);
@@ -107,38 +118,219 @@ public class ChatView extends AppCompatActivity {
             }
         });
 
-        send.setOnClickListener(new View.OnClickListener() {
+        // get sticker count
+//        DatabaseReference databaseReference2 = firebaseDatabase.getReference().child("users").child(sendername);
+//        databaseReference2.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                countSticker0 = snapshot.child("count_0").getValue(String.class);
+//                countSticker2 = snapshot.child("count_1").getValue(String.class);
+//                countSticker3 = snapshot.child("count_3").getValue(String.class);
+//                countSticker4 = snapshot.child("count_4").getValue(String.class);
+//                countSticker5 = snapshot.child("count_5").getValue(String.class);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+
+        sticker0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                enteredmessage = message.getText().toString();
-                if (enteredmessage.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "entermessage", Toast.LENGTH_SHORT).show();
-                }else{
-                    Date date = new Date();
-                    currenttime = simpleDateFormat.format(calendar.getTime());
-                    Message message1 = new Message(enteredmessage, sendername, currenttime, true);
-                    firebaseDatabase=FirebaseDatabase.getInstance();
-                    firebaseDatabase.getReference().child("chats")
-                            .child(senderroom).child("message")
-                            .push()
-                            .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    firebaseDatabase.getReference().child("chats")
-                                            .child(receiverroom).child("message")
-                                            .push()
-                                            .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
+//                Date date = new Date();
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker0", sendername, currenttime, true);
 
-                                                }
-                                            });
-                                }
-                            });
-                    message.setText(null);
-                }
+                firebaseDatabase = FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+//                                countSticker0 += 1;
+//                                firebaseDatabase.getReference().child("users").child(sendername).child("count_0").setValue(countSticker0);
+                            }
+                        });
+//                firebaseDatabase.getReference().child("users").child(sendername)
+
             }
         });
+
+        sticker1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker1", sendername, currenttime, true);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+        sticker2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker2", sendername, currenttime, true);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+        sticker3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker3", sendername, currenttime, true);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+        sticker4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker4", sendername, currenttime, true);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+        sticker5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currenttime = simpleDateFormat.format(calendar.getTime());
+                Message message1 = new Message("sticker5", sendername, currenttime, true);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                firebaseDatabase.getReference().child("chats")
+                        .child(senderroom).child("message")
+                        .push()
+                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseDatabase.getReference().child("chats")
+                                        .child(receiverroom).child("message")
+                                        .push()
+                                        .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+
+//        send.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                enteredmessage = message.getText().toString();
+//                if (enteredmessage.isEmpty()){
+//                    Toast.makeText(getApplicationContext(), "entermessage", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Date date = new Date();
+//                    currenttime = simpleDateFormat.format(calendar.getTime());
+//                    Message message1 = new Message(enteredmessage, sendername, currenttime, true);
+//                    firebaseDatabase=FirebaseDatabase.getInstance();
+//                    firebaseDatabase.getReference().child("chats")
+//                            .child(senderroom).child("message")
+//                            .push()
+//                            .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//                                    firebaseDatabase.getReference().child("chats")
+//                                            .child(receiverroom).child("message")
+//                                            .push()
+//                                            .setValue(message1).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                                @Override
+//                                                public void onComplete(@NonNull Task<Void> task) {
+//
+//                                                }
+//                                            });
+//                                }
+//                            });
+//                    message.setText(null);
+//                }
+//            }
+//        });
     }
 
     @Override
