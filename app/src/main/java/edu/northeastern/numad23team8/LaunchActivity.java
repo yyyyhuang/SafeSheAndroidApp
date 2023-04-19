d45b6ec9a925dc7007f7c6ccee86ec5c4292b5b3
Xingjian-Bi <harry1672869@gmail.com>
2023-04-19 14:36:48 -0700

    link sms to toggle button

--- app/src/main/java/edu/northeastern/numad23team8/LaunchActivity.java
+++ app/src/main/java/edu/northeastern/numad23team8/LaunchActivity.java
@@ -2,9 +2,14 @@ package edu.northeastern.numad23team8;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -15,6 +20,7 @@ import android.widget.PopupMenu;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -91,11 +97,28 @@ public class LaunchActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (start.isChecked()){
-                    Toast.makeText(LaunchActivity.this,"Shake to send SOS message",
-                            Toast.LENGTH_SHORT).show();
+                    if (ActivityCompat.checkSelfPermission(LaunchActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(LaunchActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        Intent smsIntent = new Intent(LaunchActivity.this, ShakeToSendSMS.class);
+                        smsIntent.setAction("start");
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            getApplicationContext().startForegroundService(smsIntent);
+                            Toast.makeText(LaunchActivity.this,"Shake to send SOS message",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                    }else {
+                        ActivityCompat.requestPermissions(LaunchActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 44);
+                    }
                 }else {
-                    Toast.makeText(LaunchActivity.this,"Service Stopped",
-                            Toast.LENGTH_SHORT).show();
+                    Intent smsIntent = new Intent(LaunchActivity.this, ShakeToSendSMS.class);
+                    smsIntent.setAction("STOP");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                        getApplicationContext().startForegroundService(smsIntent);
+                        getApplicationContext().stopService(smsIntent);
+                        Toast.makeText(LaunchActivity.this,"Service Stopped",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
