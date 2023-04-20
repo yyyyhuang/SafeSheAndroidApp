5bc828723730fa1a226067df64834df418090ccb
Xingjian-Bi <harry1672869@gmail.com>
2023-04-20 15:28:14 -0700

    improvement on shake to send sms feature

--- app/src/main/java/edu/northeastern/numad23team8/LaunchActivity.java
+++ app/src/main/java/edu/northeastern/numad23team8/LaunchActivity.java
@@ -13,8 +13,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,6 +27,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -45,8 +50,10 @@ public class LaunchActivity extends AppCompatActivity {
     private float accel = 10f;
     private float currAccel = SensorManager.GRAVITY_EARTH;
     private float lastAccel = SensorManager.GRAVITY_EARTH;
+    private boolean isSend = false;
+    private String emergencyNum;
 
-    private TextView curr, last;
+//    private TextView curr, last;
 
     @Override
     public void onResume(){
@@ -78,14 +85,33 @@ public class LaunchActivity extends AppCompatActivity {
 //        });
 //        if(ENum == 0){startActivity(new Intent(LaunchActivity.this,RegisterNumberActivity.class));}
 
-        curr = findViewById(R.id.textcurr);
-        last = findViewById(R.id.textlast);
+//        curr = findViewById(R.id.textcurr);
+//        last = findViewById(R.id.textlast);
 
         profile = findViewById(R.id.profile);
         emergency_contact = findViewById(R.id.emergency_contact);
         track = findViewById(R.id.track);
         start = findViewById(R.id.start);
 
+//        get emergency SMS number
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userKey = user.getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference("accounts/"+userKey+"/emergency_contact");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                emergencyNum = String.valueOf(snapshot.getValue());
+                Toast.makeText(LaunchActivity.this,"emergencyNum" + emergencyNum,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // set up sensor to detect device shake
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         SensorEventListener sensorEventListener = new SensorEventListener() {
@@ -99,12 +125,14 @@ public class LaunchActivity extends AppCompatActivity {
                     float delta = Math.abs(currAccel - lastAccel);
                     lastAccel = currAccel;
                     accel = accel * 0.9f + delta;
-                    if (accel > 12) {
-                        Toast.makeText(LaunchActivity.this,"Shake Detected",
+                    if (accel > 12 && !isSend ) {
+                        sendSMS();
+                        Toast.makeText(LaunchActivity.this,"SMS Send",
                                 Toast.LENGTH_SHORT).show();
+                        isSend = true;
                     }
-                    curr.setText("curr: " + currAccel +" last" + lastAccel);
-                    last.setText("delta: " + accel);
+//                    curr.setText("curr: " + currAccel +" last" + lastAccel);
+//                    last.setText("delta: " + accel);
                 }
             }
 
@@ -113,6 +141,35 @@ public class LaunchActivity extends AppCompatActivity {
 
             }
         };
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (start.isChecked()){
+                    if (ActivityCompat.checkSelfPermission(LaunchActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(LaunchActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(LaunchActivity.this,"Shake to send SOS message",
+                                Toast.LENGTH_SHORT).show();
+                        isSend = false;
+                        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    }else {
+                        ActivityCompat.requestPermissions(LaunchActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 44);
+                    }
+
+                }else {
+                    if (ActivityCompat.checkSelfPermission(LaunchActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(LaunchActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        sensorManager.unregisterListener(sensorEventListener, sensor);
+                        Toast.makeText(LaunchActivity.this,"Service Stopped",
+                                Toast.LENGTH_SHORT).show();
+                    }else {
+                        ActivityCompat.requestPermissions(LaunchActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 44);
+                    }
+
+                }
+            }
+        });
 
         track.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,78 +192,37 @@ public class LaunchActivity extends AppCompatActivity {
             }
         });
 
-        start.setOnClickListener(new View.OnClickListener() {
+
+
+
+    }
+    private void sendSMS(){
+
+//    private String currLocation;
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(LaunchActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(LaunchActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 44);
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
-            public void onClick(View view) {
-                if (start.isChecked()){
-//                    detectShake();
-                    sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-//                    if (ActivityCompat.checkSelfPermission(LaunchActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//                    && ContextCompat.checkSelfPermission(LaunchActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
-//                        Intent smsIntent = new Intent(LaunchActivity.this, ShakeToSendSMS.class);
-//                        smsIntent.setAction("start");
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                            getApplicationContext().startForegroundService(smsIntent);
-//                            Toast.makeText(LaunchActivity.this,"Shake to send SOS message",
-//                                    Toast.LENGTH_SHORT).show();
-//                    }
-//                    }else {
-//                        ActivityCompat.requestPermissions(LaunchActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS}, 44);
-//                    }
-
+            public void onSuccess(Location location) {
+                SmsManager smsManager = SmsManager.getDefault();
+                String currLocation;
+                if (location != null) {
+                    location.getAltitude();
+                    location.getLongitude();
+                    currLocation = "http://maps.google.com/maps?q=loc:"+location.getLatitude()+","+location.getLongitude();
                 }else {
-
-
-//                    Intent smsIntent = new Intent(LaunchActivity.this, ShakeToSendSMS.class);
-//                    smsIntent.setAction("STOP");
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-////                        getApplicationContext().startForegroundService(smsIntent);
-//                        getApplicationContext().stopService(smsIntent);
-//
-//                    }
-                    sensorManager.unregisterListener(sensorEventListener, sensor);
-                    Toast.makeText(LaunchActivity.this,"Service Stopped",
-                            Toast.LENGTH_SHORT).show();
-
-
+                    currLocation = "Location invalid";
                 }
+                smsManager.sendTextMessage(emergencyNum, null,
+                        "EMERGENCY, here's my location" + currLocation, null, null);
             }
+
+
         });
-
-
     }
-    private void detectShake(){
-//        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-//        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        SensorEventListener sensorEventListener = new SensorEventListener() {
-//            @Override
-//            public void onSensorChanged(SensorEvent sensorEvent) {
-//                if (sensorEvent != null) {
-//                    float x_val = sensorEvent.values[0];
-//                    float y_val = sensorEvent.values[1];
-//                    float z_val = sensorEvent.values[2];
-//                    currAccel = (float) Math.sqrt((double) (x_val * x_val) + (y_val * y_val) + (z_val * z_val));
-//                    float delta = Math.abs(currAccel - lastAccel);
-//                    lastAccel = currAccel;
-//                    accel = accel * 0.9f + delta;
-//                    if (accel > 12) {
-//                        Toast.makeText(LaunchActivity.this,"Shake Detected",
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                    curr.setText("curr: " + currAccel +" last" + lastAccel);
-//                    last.setText("delta: " + accel);
-//                }
-//            }
-//
-//            @Override
-//            public void onAccuracyChanged(Sensor sensor, int i) {
-//
-//            }
-//        };
-//        sensorManager.unregisterListener(sensorEventListener, sensor);
 
-    }
 
 //    public void PopupMenu(View view){
 //        PopupMenu popupMenu = new PopupMenu(LaunchActivity.this, view);
